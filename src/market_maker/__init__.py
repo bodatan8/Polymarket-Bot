62832f95c9b6eb72720e99c4769c7cb2a3d88f78
dugmojo85 <john_dugmore@hotmail.co.uk>
2026-01-23 11:31:47 -0500

    Update __init__.py exports

--- src/market_maker/__init__.py
+++ src/market_maker/__init__.py
@@ -1,1 +1,14 @@
-# 15-minute market maker module
+from src.market_maker.maker import MarketMaker, FullStackMarketMaker, FifteenMinMarketMaker
+from src.market_maker.models import FifteenMinMarket
+from src.market_maker.config import TradingConfig, config
+from src.market_maker.evaluator import MarketEvaluator
+
+__all__ = [
+    "MarketMaker",
+    "FullStackMarketMaker", 
+    "FifteenMinMarketMaker",
+    "FifteenMinMarket",
+    "TradingConfig",
+    "config",
+    "MarketEvaluator",
+]
